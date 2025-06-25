6404e156ff0040bac67440d692973c77748ee3ea
Artur <a_blyskal@wp.pl>
2025-06-25 12:28:20 +0200

    completed task

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,20 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore seatSemaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.seatSemaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        boolean seatAcquired = seatSemaphore.tryAcquire();
+        if (seatAcquired) {
+            return new BookingResult(user, true, "Booking confirmed for " + user);
+        } else {
+            return new BookingResult(user, false, "No seats available for " + user);
+        }
     }
 }
